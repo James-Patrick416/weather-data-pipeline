41cb7711cb7a820584b9ebd72aa7ca4a789ce5e8
James Patrick <jp1252211@gmail.com>
2026-07-05 14:53:27 +0300

    updated transform.py with logger

--- src/transform.py
+++ src/transform.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
 from src.database import SessionLocal, WeatherRecord
+from src.utils import logger
 
 
-def transform_and_load(weather_data: dict) -> None:
-    """
-    Transform raw weather JSON into a database record
-    and save it to SQLite.
-    """
+def transform_and_load(weather_data):
 
     session = SessionLocal()
 
     try:
+
+        logger.info("Transforming weather data.")
+
         record = WeatherRecord(
             city=weather_data["name"],
             country=weather_data["sys"]["country"],
@@ -21,18 +21,25 @@ def transform_and_load(weather_data: dict) -> None:
             weather=weather_data["weather"][0]["main"],
             description=weather_data["weather"][0]["description"],
             wind_speed=weather_data["wind"]["speed"],
-            recorded_at=datetime.fromtimestamp(weather_data["dt"]),
+            recorded_at=datetime.fromtimestamp(
+                weather_data["dt"]
+            ),
         )
 
         session.add(record)
 
         session.commit()
 
-        print("Weather record inserted successfully.")
+        logger.info("Weather record inserted into database.")
 
-    except Exception:
+    except Exception as err:
+
         session.rollback()
+
+        logger.exception(f"Database transaction failed: {err}")
+
         raise
 
     finally:
+
         session.close()

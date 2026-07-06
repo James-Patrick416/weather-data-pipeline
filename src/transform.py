65d8532c70808f832440f402d583c6f3a639fdd0
James Patrick <jp1252211@gmail.com>
2026-07-06 15:15:19 +0300

    updated api.py with pydantic validation

--- src/transform.py
+++ src/transform.py
@@ -1,29 +1,40 @@
+"""
+Transform validated weather data and load it into the database.
+"""
+
 from datetime import datetime
 
 from src.database import SessionLocal, WeatherRecord
+from src.models import WeatherResponse
 from src.utils import logger
 
 
-def transform_and_load(weather_data):
+def transform_and_load(weather: WeatherResponse) -> None:
+    """
+    Transform a validated WeatherResponse object into
+    a WeatherRecord database row.
+
+    Args:
+        weather:
+            Validated weather data from the API.
+    """
 
     session = SessionLocal()
 
     try:
 
-        logger.info("Transforming weather data.")
+        logger.info("Transforming validated weather data.")
 
         record = WeatherRecord(
-            city=weather_data["name"],
-            country=weather_data["sys"]["country"],
-            temperature=weather_data["main"]["temp"],
-            humidity=weather_data["main"]["humidity"],
-            pressure=weather_data["main"]["pressure"],
-            weather=weather_data["weather"][0]["main"],
-            description=weather_data["weather"][0]["description"],
-            wind_speed=weather_data["wind"]["speed"],
-            recorded_at=datetime.fromtimestamp(
-                weather_data["dt"]
-            ),
+            city=weather.name,
+            country=weather.sys.country,
+            temperature=weather.main.temp,
+            humidity=weather.main.humidity,
+            pressure=weather.main.pressure,
+            weather=weather.weather[0].main,
+            description=weather.weather[0].description,
+            wind_speed=weather.wind.speed,
+            recorded_at=datetime.fromtimestamp(weather.dt),
         )
 
         session.add(record)
